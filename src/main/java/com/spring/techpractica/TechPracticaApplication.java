f2e84ead896f5873c5dce9b2bc07bbf912af5272
MohammedMamounHamedAlhaj <134335033+M0hammedAlhaj@users.noreply.github.com>
2025-04-08 21:15:43 +0300

    create architecture for a project.

---
+++ src/main/java/com/spring/techpractica/TechPracticaApplication.java
@@ -0,0 +1,13 @@
+package com.spring.techpractica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechPracticaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechPracticaApplication.class, args);
+    }
+
+}
