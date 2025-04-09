b0a2591a5b84f38b610d13154f15cd48ed80f492
aboodtouq <aboodtamer60@gmail.com>
2025-04-09 22:40:07 +0300

    adding some models and adding data fields

--- src/main/java/com/spring/techpractica/model/Task.java
+++ src/main/java/com/spring/techpractica/model/Task.java
@@ -1,4 +1,27 @@
 package com.spring.techpractica.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity(name = "Task")
+@Getter
+@Setter
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "task_id")
+    private int taskId;
+    @Column(name = "task_name")
+    private String taskName;
+    @Column(name = "task_description")
+    private String taskDescription;
+    @Column(name = "task_deadline")
+    private int taskDeadline;
+    @Column(name = "session_id")
+    private int sessionId;
+    @Column(name = "user_owner_id")
+    private String userOwnerId;
 }
