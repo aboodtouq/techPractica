8f67fad4bacb68a695da37613a6ada17040679f7
qandeel <mohaqan2004@gmail.com>
2025-04-09 13:24:10 +0300

    edit sessions class

--- src/main/java/com/spring/techpractica/model/Sessions.java
+++ src/main/java/com/spring/techpractica/model/Sessions.java
@@ -1,8 +1,13 @@
 package com.spring.techpractica.model;
 
 public class Sessions {
+
     private int sessionId;
+
     private String sessionName;
+
     private String sessionDescription;
+
     private boolean isPrivate;
+
 }
