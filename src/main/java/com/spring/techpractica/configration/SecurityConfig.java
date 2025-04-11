7efc97c02be0f33164edeba5b75b25fb5658bcb5
qandeel <mohaqan2004@gmail.com>
2025-04-11 23:24:04 +0300

    updates

--- src/main/java/com/spring/techpractica/configration/SecurityConfig.java
+++ src/main/java/com/spring/techpractica/configration/SecurityConfig.java
@@ -2,8 +2,12 @@ package com.spring.techpractica.configration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,7 +19,13 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.build();
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/**").permitAll()
+                        )
+                        .httpBasic(Customizer.withDefaults())
+                        .build();
+
     }
 
     @Bean
