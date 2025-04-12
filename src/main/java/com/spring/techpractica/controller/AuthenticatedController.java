5fe166b2776f0b7e2434a338a2a816c0dc7e48f5
MohammedMamounHamedAlhaj <134335033+M0hammedAlhaj@users.noreply.github.com>
2025-04-12 18:59:58 +0300

    Add ExceptionHandlerController

--- src/main/java/com/spring/techpractica/controller/AuthenticatedController.java
+++ src/main/java/com/spring/techpractica/controller/AuthenticatedController.java
@@ -4,8 +4,8 @@ import com.spring.techpractica.dto.UserCreateAccount;
 import com.spring.techpractica.dto.UserLogin;
 import com.spring.techpractica.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,13 +20,13 @@ public class AuthenticatedController {
     }
 
     @PostMapping("/registration")
-    public ResponseEntity<String> createAccount(@ModelAttribute UserCreateAccount userCreateAccount) {
+    public ResponseEntity<String> createAccount(@RequestBody UserCreateAccount userCreateAccount) {
         userService.createAccount(userCreateAccount);
         return ResponseEntity.ok("Create Account Successful");
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@ModelAttribute UserLogin userLogin) {
+    public ResponseEntity<String> login(@RequestBody UserLogin userLogin) {
         userService.userLogin(userLogin);
         return ResponseEntity.ok("Login Successful");
     }
