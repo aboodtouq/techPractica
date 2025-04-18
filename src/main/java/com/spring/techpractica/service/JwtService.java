a790fd04b19dae1c8a822b705f48056de2339b24
MohammedMamounHamedAlhaj <134335033+M0hammedAlhaj@users.noreply.github.com>
2025-04-18 16:10:21 +0300

    edit jwt generation for more secure

--- src/main/java/com/spring/techpractica/service/JwtService.java
+++ src/main/java/com/spring/techpractica/service/JwtService.java
@@ -3,12 +3,12 @@ package com.spring.techpractica.service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
-import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
-import java.security.NoSuchAlgorithmException;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,12 +17,12 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-    private SecretKey secretKey;
+    private final SecretKey secretKey;
 
 
-    public JwtService() throws NoSuchAlgorithmException {
-        KeyGenerator keyGenerator = KeyGenerator.getInstance("HmacSHA256");
-        secretKey = keyGenerator.generateKey();
+    public JwtService() {
+        String key = "2XwD8eRxMvT4n9VpLF9UyBG5cD7NlKmKqY6fO9qxMzU="; // Example key (should be 256 bits and secure)
+        this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(key));
     }
 
     public String generateToken(String email) {
