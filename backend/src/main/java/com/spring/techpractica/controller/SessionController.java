f2b4997a84f39b93a4f75469410f2910f94c3711
MohammedMamounHamedAlhaj <134335033+M0hammedAlhaj@users.noreply.github.com>
2025-04-22 18:33:07 +0300

    fix bug

--- backend/src/main/java/com/spring/techpractica/controller/SessionController.java
+++ backend/src/main/java/com/spring/techpractica/controller/SessionController.java
@@ -9,12 +9,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-=======
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
