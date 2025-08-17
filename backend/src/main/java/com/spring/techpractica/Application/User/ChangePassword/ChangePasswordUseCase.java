744ded8613a62d38a399fcb39ec034dbf84e52d5
M0hammedAlhaj <2232797@std.hu.edu.jo>
2025-08-17 16:41:51 +0300

    feat(backend):create user service and change password

---
+++ backend/src/main/java/com/spring/techpractica/Application/User/ChangePassword/ChangePasswordUseCase.java
@@ -0,0 +1,27 @@
+package com.spring.techpractica.Application.User.ChangePassword;
+
+import com.spring.techpractica.Core.Shared.Exception.ResourcesNotFoundException;
+import com.spring.techpractica.Core.User.Service.UserService;
+import com.spring.techpractica.Core.User.User;
+import com.spring.techpractica.Core.User.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@AllArgsConstructor
+public class ChangePasswordUseCase {
+    private final UserRepository userRepository;
+
+    private final UserService userService;
+
+    @Transactional
+    public User execute(ChangePasswordCommand command) {
+        User user = userRepository.findById(command.id())
+                .orElseThrow(() -> new ResourcesNotFoundException(command.id()));
+
+        userService.changePassword(user, command.password());
+        userRepository.update(user);
+        return user;
+    }
+}
