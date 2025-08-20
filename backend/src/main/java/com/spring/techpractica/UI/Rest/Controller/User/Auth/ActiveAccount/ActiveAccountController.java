6abc8618c32fd17aae31630033a1c6dc2fe41ec9
M0hammedAlhaj <2232797@std.hu.edu.jo>
2025-08-20 13:18:13 +0300

    doc(backend):add swagger doc in endpoint

--- backend/src/main/java/com/spring/techpractica/UI/Rest/Controller/User/Auth/ActiveAccount/ActiveAccountController.java
+++ backend/src/main/java/com/spring/techpractica/UI/Rest/Controller/User/Auth/ActiveAccount/ActiveAccountController.java
@@ -6,6 +6,10 @@ import com.spring.techpractica.Core.User.User;
 import com.spring.techpractica.UI.Rest.Resources.User.UserResources;
 import com.spring.techpractica.UI.Rest.Shared.StandardSuccessResponse;
 import com.spring.techpractica.infrastructure.Jwt.JwtExtracting;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,16 +20,34 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
+
 @RestController
 @RequestMapping("/api/v1/auth")
 @AllArgsConstructor
+@Tag(name = "Authentication", description = "Endpoints related to user authentication and account activation")
 public class ActiveAccountController {
 
     private final JwtExtracting jwtExtracting;
     private final ActiveAccountUseCase useCase;
 
+    /**
+     * Activates a user account based on the provided JWT token.
+     *
+     * @param token JWT token containing the user's ID
+     * @return ResponseEntity containing the activated user details
+     */
+    @Operation(
+            summary = "Activate User Account",
+            description = "Verifies the provided JWT token and activates the corresponding user account."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "202", description = "Account successfully activated"),
+            @ApiResponse(responseCode = "400", description = "Invalid token provided"),
+            @ApiResponse(responseCode = "404", description = "User not found")
+    })
     @GetMapping("/active-account")
     public ResponseEntity<?> verifyToken(@RequestParam String token) {
+
         UUID id = jwtExtracting.extractId(token);
         User user = useCase.execute(new ActiveAccountCommand(id));
 
@@ -37,4 +59,5 @@ public class ActiveAccountController {
                         .build());
     }
 }
+
 
