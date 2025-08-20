fc2105a56d9e8bcff63ea8bb878f4e6dd1b02b29
M0hammedAlhaj <2232797@std.hu.edu.jo>
2025-08-20 12:51:16 +0300

    refact(backend):change response

--- backend/src/main/java/com/spring/techpractica/UI/Rest/Controller/User/Auth/ActiveAccount/ActiveAccountController.java
+++ backend/src/main/java/com/spring/techpractica/UI/Rest/Controller/User/Auth/ActiveAccount/ActiveAccountController.java
@@ -2,31 +2,39 @@ package com.spring.techpractica.UI.Rest.Controller.User.Auth.ActiveAccount;
 
 import com.spring.techpractica.Application.User.AcvtiveAccount.ActiveAccountCommand;
 import com.spring.techpractica.Application.User.AcvtiveAccount.ActiveAccountUseCase;
+import com.spring.techpractica.Core.User.User;
+import com.spring.techpractica.UI.Rest.Resources.User.UserResources;
+import com.spring.techpractica.UI.Rest.Shared.StandardSuccessResponse;
 import com.spring.techpractica.infrastructure.Jwt.JwtExtracting;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @AllArgsConstructor
 public class ActiveAccountController {
+
     private final JwtExtracting jwtExtracting;
     private final ActiveAccountUseCase useCase;
 
     @GetMapping("/active-account")
-    public void verifyToken(@RequestParam String token,
-                            HttpServletResponse response) throws IOException {
+    public ResponseEntity<?> verifyToken(@RequestParam String token) {
         UUID id = UUID.fromString(jwtExtracting.extractId(token));
-        useCase.execute(new ActiveAccountCommand(id));
-        try {
-            response.sendRedirect("http://localhost:3000/User");
-        } catch (Exception e) {
-            response.sendRedirect("http//localhost:3030/not-found");
-        }
+        User user = useCase.execute(new ActiveAccountCommand(id));
+
+        return ResponseEntity.status(HttpStatus.ACCEPTED.value())
+                .body(StandardSuccessResponse.builder()
+                        .data(new UserResources(user))
+                        .message("Active account has been verified")
+                        .status(HttpStatus.ACCEPTED.value())
+                        .build());
     }
 }
 
