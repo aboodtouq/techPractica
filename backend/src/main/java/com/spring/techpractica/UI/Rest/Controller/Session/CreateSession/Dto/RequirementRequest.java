678466f95b7ab76648ed8a8605adcb98119321c3
qandeel <mohaqan2004@gmail.com>
2025-08-22 19:54:14 +0300

    fix bug

--- backend/src/main/java/com/spring/techpractica/UI/Rest/Controller/Session/CreateSession/Dto/RequirementRequest.java
+++ backend/src/main/java/com/spring/techpractica/UI/Rest/Controller/Session/CreateSession/Dto/RequirementRequest.java
@@ -2,11 +2,13 @@ package com.spring.techpractica.UI.Rest.Controller.Session.CreateSession.Dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @AllArgsConstructor
 @Getter
+@NoArgsConstructor
 public class RequirementRequest {
 
     private String fieldName;
