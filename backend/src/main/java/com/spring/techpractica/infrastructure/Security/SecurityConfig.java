19fe3200e8789d0f5bed00235cf1161bf0ce2b39
M0hammedAlhaj <2232797@std.hu.edu.jo>
2025-08-25 20:24:03 +0300

    fix(backend):endpoint authentication

--- backend/src/main/java/com/spring/techpractica/infrastructure/Security/SecurityConfig.java
+++ backend/src/main/java/com/spring/techpractica/infrastructure/Security/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.spring.techpractica.infrastructure.Security.Filter.JwtFilter;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -25,7 +26,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/sessions/", "POST").authenticated()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/sessions/").authenticated()
                         .anyRequest().permitAll())
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
