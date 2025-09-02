afdad89e4c645a26a6834015d4efd7e525aef2f9
mhmdqandeel <163772594+mhmdqandeel@users.noreply.github.com>
2025-09-02 15:02:32 +0300

    return technologies as plain string list in requirement response (#101)
    * return technologies as plain string list in requirement response
    
    * add requirementId to create session response

--- backend/src/main/java/com/spring/techpractica/UI/Rest/Resources/Requirment/RequirementResources.java
+++ backend/src/main/java/com/spring/techpractica/UI/Rest/Resources/Requirment/RequirementResources.java
@@ -4,16 +4,19 @@ import com.spring.techpractica.Core.Requirement.Entity.Requirement;
 import com.spring.techpractica.UI.Rest.Resources.RequirementTechnology.RequirementTechnologyCollection;
 import lombok.Getter;
 
+import java.util.UUID;
+
 @Getter
 public class RequirementResources {
 
+    private final UUID requirementId;
     private final String field;
     private final RequirementTechnologyCollection technologies;
 
     public RequirementResources(Requirement requirement) {
         this.field = requirement.getField().getName();
-
         this.technologies =
                 new RequirementTechnologyCollection(requirement.getRequirementTechnologies());
+        this.requirementId = requirement.getId();
     }
 }
