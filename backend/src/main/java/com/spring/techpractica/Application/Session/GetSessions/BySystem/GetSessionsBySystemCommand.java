eae022e6afa01e71ea9f1a52ae24ab97d5967fdb
aboodtouq <aboodtamer60@gmail.com>
2025-09-23 12:06:32 +0300

    delete the by system files

--- backend/src/main/java/com/spring/techpractica/Application/Session/GetSessions/BySystem/GetSessionsBySystemCommand.java
+++
@@ -1,8 +0,0 @@
-package com.spring.techpractica.Application.Session.GetSessions.BySystem;
-
-import java.util.UUID;
-
-public record GetSessionsBySystemCommand(UUID systemId,
-                                         int size,
-                                         int page) {
-}
