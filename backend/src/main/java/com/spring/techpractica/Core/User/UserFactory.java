ca2fcad2a1ee89c69e2d1d43b2fc16c1cddf9cb5
mhmdqandeel <mohaqan2004@gmail.com>
2025-10-11 23:30:15 +0300

    add default role for userFactory

--- backend/src/main/java/com/spring/techpractica/Core/User/UserFactory.java
+++ backend/src/main/java/com/spring/techpractica/Core/User/UserFactory.java
@@ -2,6 +2,8 @@ package com.spring.techpractica.Core.User;
 
 import com.spring.techpractica.Core.Role.Entity.Role;
 import com.spring.techpractica.Core.Role.Model.RoleType;
+import com.spring.techpractica.Core.Role.RoleRepository;
+import com.spring.techpractica.Core.Shared.Exception.ResourcesNotFoundException;
 import com.spring.techpractica.Core.User.Service.PasswordEncryptor;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -11,18 +13,23 @@ import java.util.List;
 @Component
 @AllArgsConstructor
 public class UserFactory {
+
     private final PasswordEncryptor passwordEncryptor;
+    private final RoleRepository roleRepository;
 
     public User create(String name,
                        String email,
                        String password) {
         String encryptedPassword = passwordEncryptor.hash(password);
 
+        Role userRole = roleRepository.findByRoleType(RoleType.ROLE_USER)
+                .orElseThrow(() -> new ResourcesNotFoundException(RoleType.ROLE_USER.toString()));
+
         return User.builder()
                 .name(name)
                 .email(email)
                 .password(encryptedPassword)
-                .roles(List.of(new Role(RoleType.ROLE_USER)))
+                .roles(List.of(userRole))
                 .build();
     }
 }
