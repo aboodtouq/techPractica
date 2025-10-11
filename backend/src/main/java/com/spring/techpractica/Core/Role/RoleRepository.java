5153a7265324202af0b6b64c137e1197841c46dc
mhmdqandeel <mohaqan2004@gmail.com>
2025-10-11 23:17:23 +0300

    add role repo

---
+++ backend/src/main/java/com/spring/techpractica/Core/Role/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.spring.techpractica.Core.Role;
+
+import com.spring.techpractica.Core.Role.Entity.Role;
+import com.spring.techpractica.Core.Shared.BaseRepository;
+
+public interface RoleRepository extends BaseRepository<Role> {
+}
