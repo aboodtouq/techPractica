50b69e19d27636babd829df428e4fc8c4c5dbe5f
mhmdqandeel <mohaqan2004@gmail.com>
2025-10-18 16:47:27 +0300

    add CreateTaskController

---
+++ backend/src/main/java/com/spring/techpractica/UI/Rest/Controller/Task/CreateTaskController.java
@@ -0,0 +1,11 @@
+package com.spring.techpractica.UI.Rest.Controller.Task;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/tasks")
+@AllArgsConstructor
+public class CreateTaskController {
+}
