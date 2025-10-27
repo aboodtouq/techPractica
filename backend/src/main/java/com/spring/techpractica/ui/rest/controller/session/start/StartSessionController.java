80d03e87de9765afdba72c95eb265ce5d290f6a4
mhmdqandeel <mohaqan2004@gmail.com>
2025-10-27 22:37:24 +0300

    add http varible

--- backend/src/main/java/com/spring/techpractica/ui/rest/controller/session/start/StartSessionController.java
+++ backend/src/main/java/com/spring/techpractica/ui/rest/controller/session/start/StartSessionController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +24,7 @@ public class StartSessionController {
 
     private final StartSessionUseCase startSessionUseCase;
 
+    @PutMapping("/start")
     public ResponseEntity<?> invoke(@AuthenticationPrincipal UserAuthentication userAuthentication,
                                     @PathVariable UUID sessionId) {
         Session session = startSessionUseCase.execute(
