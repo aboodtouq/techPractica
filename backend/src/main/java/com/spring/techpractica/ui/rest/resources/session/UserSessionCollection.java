4c62f7b7d7e9f4ebef43f4a37387493e4bf1bf1b
aboodtouq <aboodtamer60@gmail.com>
2025-11-12 18:31:41 +0300

    making new resources and collection for thr user sessions

---
+++ backend/src/main/java/com/spring/techpractica/ui/rest/resources/session/UserSessionCollection.java
@@ -0,0 +1,41 @@
+package com.spring.techpractica.ui.rest.resources.session;
+
+import com.spring.techpractica.core.session.entity.Session;
+import com.spring.techpractica.core.session.members.model.Role;
+import lombok.Getter;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Getter
+public class UserSessionCollection {
+
+    private final List<UserSessionResources> sessions;
+
+    private final long totalItems;
+
+    private final int totalPages;
+
+    private final int pageSize=6;
+
+
+    public UserSessionCollection(List<Session> sessions, long totalItems, UUID userId) {
+        this.sessions = (sessions == null ? java.util.List.<Session>of() : sessions)
+                .stream()
+                .map(s -> new UserSessionResources(
+                        s,
+                        (s.getOwner().getId().equals(userId))
+                                ? Role.OWNER
+                                : Role.PARTICIPATE
+                ))
+                .collect(Collectors.toList());
+
+        this.totalItems = totalItems;
+        this.totalPages = (int) Math.ceil((double) totalItems / pageSize);
+
+    }
+
+
+}
--- backend/src/main/java/com/spring/techpractica/ui/rest/resources/session/UserSessionResources.java
+++ backend/src/main/java/com/spring/techpractica/ui/rest/resources/session/UserSessionResources.java
@@ -1,7 +1,7 @@
 package com.spring.techpractica.ui.rest.resources.session;
 
-import com.spring.techpractica.core.session.entity.Session;
 import com.spring.techpractica.core.session.SessionStatus;
+import com.spring.techpractica.core.session.entity.Session;
 import com.spring.techpractica.core.session.members.model.Role;
 import com.spring.techpractica.ui.rest.resources.requirement.RequirementCollection;
 import com.spring.techpractica.ui.rest.resources.system.SystemResources;
@@ -14,7 +14,7 @@ import java.util.UUID;
 @Getter
 @Builder
 @AllArgsConstructor
-public class SessionResources {
+public class UserSessionResources {
 
     private UUID id;
     private String name;
@@ -25,9 +25,10 @@ public class SessionResources {
     private RequirementCollection requirements;
     private UUID ownerId;
     private String ownerFullName;
+    private Role role;
 
 
-    public SessionResources(Session session) {
+    public UserSessionResources(Session session,Role role) {
         this.id = session.getId();
         this.name = session.getName();
         this.description = session.getDescription();
@@ -35,6 +36,7 @@ public class SessionResources {
         this.status = session.getStatus();
         this.ownerId = session.getOwnerId();
         this.ownerFullName = session.getOwnerFullName();
+        this.role = role;
 
         if (session.getSystems() != null && !session.getSystems().isEmpty()) {
             this.system = new SystemResources(session.getSystems().getLast());
