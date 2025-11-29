3e75fe60a6c0ce572354a4305b0d4b09c7ae48a7
aboodtouq <aboodtamer60@gmail.com>
2025-11-29 14:06:07 +0300

    making the task returend

---
+++ backend/src/main/java/com/spring/techpractica/ui/rest/controller/session/task/get/GetTaskController.java
@@ -0,0 +1,84 @@
+package com.spring.techpractica.ui.rest.controller.session.task.get;
+
+import com.spring.techpractica.application.session.task.delete.DeleteTaskCommand;
+import com.spring.techpractica.application.session.task.delete.DeleteTaskUseCase;
+import com.spring.techpractica.application.session.task.get.GetTaskCommand;
+import com.spring.techpractica.application.session.task.get.GetTaskUseCase;
+import com.spring.techpractica.core.task.entity.Task;
+import com.spring.techpractica.core.user.UserAuthentication;
+import com.spring.techpractica.ui.rest.resources.task.TaskCollection;
+import com.spring.techpractica.ui.rest.resources.task.TaskResources;
+import com.spring.techpractica.ui.rest.shared.StandardSuccessResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/sessions/tasks")
+@AllArgsConstructor
+public class GetTaskController {
+
+    private final GetTaskUseCase getTaskUseCase;
+
+    @Operation(
+            summary = "Get Tasks",
+            description = "Get the tasks in the session. "
+                    + "The user must be session participant. Returns the all Tasks in the session ."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Tasks returned successfully",
+                    content = @Content(schema = @Schema(implementation = StandardSuccessResponse.class))
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid request (e.g. malformed UUID)",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "Unauthorized (invalid credentials)",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Forbidden (user is not the owner of the session)",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Task not found",
+                    content = @Content
+            )
+    })
+    @GetMapping("/")
+    public ResponseEntity<?> getTask(
+            @AuthenticationPrincipal UserAuthentication userAuthentication,
+            @RequestBody GetTaskRequest request) {
+
+        List<Task> tasks = getTaskUseCase.execute(new GetTaskCommand(
+                userAuthentication.getUserId(),
+                request.sessionId()));
+
+        TaskCollection responseData = new TaskCollection(tasks);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(
+                        StandardSuccessResponse.<TaskCollection>builder()
+                                .data(responseData)
+                                .message("Tasks Get successfully")
+                                .status(HttpStatus.OK.value())
+                                .build()
+                );
+    }
+}
