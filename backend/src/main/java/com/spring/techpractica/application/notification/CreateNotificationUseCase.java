e0936fc99a074a587944553ed7a8ce11b105066f
mhmdqandeel <mohaqan2004@gmail.com>
2025-12-03 16:39:34 +0300

    complete notificatuon use case

--- backend/src/main/java/com/spring/techpractica/application/notification/CreateNotificationUseCase.java
+++ backend/src/main/java/com/spring/techpractica/application/notification/CreateNotificationUseCase.java
@@ -1,16 +1,19 @@
 package com.spring.techpractica.application.notification;
 
+import com.spring.techpractica.core.notification.NotificationRepository;
 import com.spring.techpractica.core.notification.entity.Notification;
 import com.spring.techpractica.core.user.User;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class CreateNotificationUseCase {
 
-
+    private final NotificationRepository notificationRepository;
 
     public Notification execute(User user, String title, String content) {
         Notification notification = new Notification(title, content, user, false);
-
+        return notificationRepository.save(notification);
     }
 }
