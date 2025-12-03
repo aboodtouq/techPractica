1b9df505a816f57c0eb71191208ac1f4fac984de
mhmdqandeel <mohaqan2004@gmail.com>
2025-12-03 15:32:24 +0300

    add notification service and title prop in Notification class

---
+++ backend/src/main/java/com/spring/techpractica/application/notification/CreateNotificationUseCase.java
@@ -0,0 +1,14 @@
+package com.spring.techpractica.application.notification;
+
+import com.spring.techpractica.core.notification.entity.Notification;
+import com.spring.techpractica.core.user.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateNotificationUseCase {
+
+
+    public Notification execute(User user, String title, String body) {
+
+    }
+}
