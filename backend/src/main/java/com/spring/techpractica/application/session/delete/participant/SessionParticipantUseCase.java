643847e3b3491646616d77021fc6e67cc129d217
mhmdqandeel <mohaqan2004@gmail.com>
2025-12-24 17:28:01 +0300

    complete implementaion of delete session participant

--- backend/src/main/java/com/spring/techpractica/application/session/delete/participant/SessionParticipantUseCase.java
+++ backend/src/main/java/com/spring/techpractica/application/session/delete/participant/SessionParticipantUseCase.java
@@ -1,9 +1,30 @@
 package com.spring.techpractica.application.session.delete.participant;
 
+import com.spring.techpractica.core.session.SessionRepository;
+import com.spring.techpractica.core.session.entity.Session;
+import com.spring.techpractica.core.shared.Exception.UnauthorizedActionException;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class SessionParticipantUseCase {
 
+    private final SessionRepository sessionRepository;
 
+    public void execute(DeleteSessionParticipantCommand command) {
+        Session session = sessionRepository.getOrThrowByID(command.sessionId());
+
+        if (!session.isOwner(command.ownerId())) {
+            throw new UnauthorizedActionException("User must be the session owner to delete participant");
+        }
+
+        if (command.ownerId().equals(command.participantId())) {
+            throw new UnauthorizedActionException("Owner cannot be removed from session");
+        }
+
+        session.removeMember(command.participantId());
+
+        sessionRepository.save(session);
+    }
 }
--- backend/src/main/java/com/spring/techpractica/ui/rest/controller/session/delete/participant/SessionParticipantController.java
+++ backend/src/main/java/com/spring/techpractica/ui/rest/controller/session/delete/participant/SessionParticipantController.java
@@ -1,6 +1,10 @@
 package com.spring.techpractica.ui.rest.controller.session.delete.participant;
 
+import com.spring.techpractica.application.session.delete.participant.DeleteSessionParticipantCommand;
+import com.spring.techpractica.application.session.delete.participant.SessionParticipantUseCase;
 import com.spring.techpractica.core.user.UserAuthentication;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,7 +16,10 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/session/")
+@AllArgsConstructor
 public class SessionParticipantController {
+
+    private final SessionParticipantUseCase sessionParticipantUseCase;
 
     @DeleteMapping("/{sessionId}/participants/{participantId}")
     public ResponseEntity<?> deleteSessionParticipant(@AuthenticationPrincipal UserAuthentication  user,
@@ -20,5 +27,10 @@ public class SessionParticipantController {
                                                       @PathVariable UUID participantId) {
         UUID ownerId = user.getUserId();
 
+        sessionParticipantUseCase.execute(new DeleteSessionParticipantCommand(ownerId, participantId, sessionId));
+
+        return ResponseEntity.ok("Delete Session participant executed successfully")
+                .status(HttpStatus.OK)
+                .build();
     }
 }
