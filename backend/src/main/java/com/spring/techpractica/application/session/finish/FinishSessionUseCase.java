73d3d4b43234c0f792c715a929e030e3343865e1
mhmdqandeel <mohaqan2004@gmail.com>
2025-12-25 14:32:55 +0300

    complete finish session controller

--- backend/src/main/java/com/spring/techpractica/application/session/finish/FinishSessionUseCase.java
+++ backend/src/main/java/com/spring/techpractica/application/session/finish/FinishSessionUseCase.java
@@ -10,7 +10,6 @@ import com.spring.techpractica.core.session.entity.statistics.UserTaskStatistics
 import com.spring.techpractica.core.shared.Exception.UnauthorizedActionException;
 import com.spring.techpractica.core.task.model.TaskStatus;
 import com.spring.techpractica.core.user.User;
-import com.spring.techpractica.core.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
