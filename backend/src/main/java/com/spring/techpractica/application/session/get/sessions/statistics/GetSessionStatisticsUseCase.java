a5387008d18617cb92ec217f5ec35e3fd614fabe
mhmdqandeel <mohaqan2004@gmail.com>
2025-12-25 18:45:48 +0300

    complete impl of GetSessionStatisticsUseCase

--- backend/src/main/java/com/spring/techpractica/application/session/get/sessions/statistics/GetSessionStatisticsUseCase.java
+++ backend/src/main/java/com/spring/techpractica/application/session/get/sessions/statistics/GetSessionStatisticsUseCase.java
@@ -1,8 +1,11 @@
 package com.spring.techpractica.application.session.get.sessions.statistics;
 
 
+import com.spring.techpractica.core.session.SessionRepository;
 import com.spring.techpractica.core.session.SessionStatisticsRepository;
+import com.spring.techpractica.core.session.entity.Session;
 import com.spring.techpractica.core.session.entity.statistics.SessionStatistics;
+import com.spring.techpractica.core.shared.Exception.UnauthorizedActionException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +14,15 @@ import org.springframework.stereotype.Service;
 public class GetSessionStatisticsUseCase {
 
     private final SessionStatisticsRepository  sessionStatisticsRepository;
+    private final SessionRepository  sessionRepository;
 
     public SessionStatistics execute(GetSessionStatisticsCommand command) {
+        Session session = sessionRepository.getOrThrowByID(command.sessionId());
 
+        if (!session.isMember(command.userId())) {
+            throw new UnauthorizedActionException("You are not member on this session");
+        }
+
+        return sessionStatisticsRepository.findBySessionId(command.sessionId());
     }
 }
--- backend/src/main/java/com/spring/techpractica/core/session/SessionStatisticsRepository.java
+++ backend/src/main/java/com/spring/techpractica/core/session/SessionStatisticsRepository.java
@@ -3,5 +3,9 @@ package com.spring.techpractica.core.session;
 import com.spring.techpractica.core.session.entity.statistics.SessionStatistics;
 import com.spring.techpractica.core.shared.BaseRepository;
 
+import java.util.UUID;
+
 public interface SessionStatisticsRepository extends BaseRepository<SessionStatistics> {
+
+    SessionStatistics findBySessionId(UUID sessionId);
 }
